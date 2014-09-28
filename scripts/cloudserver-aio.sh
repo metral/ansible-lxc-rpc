7411c3bda123834ac4501b49d016dbcdd5d720ee
Mike Metral <metral@gmail.com>
2014-09-28 14:18:39 -0700

    use metral fork

--- scripts/cloudserver-aio.sh
+++ scripts/cloudserver-aio.sh
@@ -14,8 +14,8 @@
 # limitations under the License.
 set -e -u -v -x
 
-REPO_URL=${REPO_URL:-"https://github.com/rcbops/ansible-lxc-rpc.git"}
-REPO_BRANCH=${REPO_BRANCH:-"master"}
+REPO_URL=${REPO_URL:-"https://github.com/metral/ansible-lxc-rpc.git"}
+REPO_BRANCH=${REPO_BRANCH:-"stable/icehouse"}
 FROZEN_REPO_URL=${FROZEN_REPO_URL:-"http://rpc-slushee.rackspace.com"}
 MAX_RETRIES=${MAX_RETRIES:-5}
 
